e24ca01e5ba35ab9db051a27c1b123956aa37480
JanLascala <lascala.jan@gmail.com>
2025-02-11 13:41:20 +0100

    Add calculation

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const mess5 = "buzz"
 
 for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 === 0) {
-        console.log(i + "" + mess3)
+    if (i % 3 + i % 5 === 0) {
+        console.log(i + "" + mess3 + mess5)
     }
 
 
