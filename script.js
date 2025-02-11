555ab0bc12d2c23de5f64eb7a5d817747bf7d6c1
JanLascala <lascala.jan@gmail.com>
2025-02-11 15:18:11 +0100

    Initial

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ const mess5 = "buzz"
 
 
 for (let i = 1; i <= 100; i++) {
-
-    if (i % 3 === 0) {
-        console.log(i + "" + mess3)
+    if (i % 3 + i % 5 === 0) {
+        console.log(i + "" + mess3 + mess5);
+    } else if (i % 5 === 0) {
+        console.log(i + "" + mess5);
+    } else (i % 3 === 0) {
+        console.log(i + "" + mess3);
     }
+}
+
+
+
+
 
 
 
