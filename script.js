37bb81890b5b5aa2727c234b23c6c03b4a7b573f
JanLascala <lascala.jan@gmail.com>
2025-02-11 15:51:10 +0100

    Progress made

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const mess5 = "buzz"
 for (let i = 1; i <= 100; i++) {
     if (i % 3 + i % 5 === 0) {
         console.log(i + "" + mess3 + mess5);
-    } else if (i % 5 === 0) {
+    } else if (i % 5 === 0 || i % 3 === 0) {
         console.log(i + "" + mess5);
-    } else (i % 3 === 0)
-    console.log(i + "" + mess3);
-
+    } else {
+        console.log(i)
+    }
 }
+
+
 
 
 
