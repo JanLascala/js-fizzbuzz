48abb3219285e1bcbae4ba8468fb2eaae10933d3
JanLascala <lascala.jan@gmail.com>
2025-02-11 15:02:40 +0100

    Reset

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ console.log("work")
 
 const mess3 = "fizz"
 const mess5 = "buzz"
-const i = 0
 
-/*const mult_3 = "3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 75, 78, 81, 84, 87, 93, 96, 99"
-const mult_5 = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,]
-const mult_3_5 = [15, 30, 45, 60, 90,]*/
 
-while (i < 100) {
-    console.log(i + mess3)
-}
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0) {
+        console.log(i + "" + mess3)
+    }
+
+
+
+
 
